52b1ad89d98b29b485cd23da5d8c109c4e5dd7ce
Andrew G <vardantexcode@gmail.com>
2022-06-22 14:56:03 -0400

    alias Datatype calling

---
+++ aliasDatatype.ts
@@ -0,0 +1,11 @@
+
+//Declare a type of an array to call 
+type arrayString = Array<string>;
+type arrayNumber = Array<number>;
+type arrayAny = Array<any>;
+
+
+let words : arrayString = ["A", "B", "C"]
+let numbers : arrayNumber = [1, 2, 3]
+let anything : arrayAny = [1, "Two", false]
+
