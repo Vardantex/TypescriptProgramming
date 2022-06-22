bbb5209e6eb5a00a5d6ffc6642439c3096d12580
Andrew G <vardantexcode@gmail.com>
2022-06-22 14:20:20 -0400

    Datatypes implementation

---
+++ Datatypes.ts
@@ -0,0 +1,12 @@
+
+// boolean 
+let bool: boolean = true;
+
+//array 
+var anyType: any[] = [1, "two", true] 
+var stringType: string[] = ["A", "B"]
+var numberType: number[] = [1, 2, 3]
+
+//Tuple: mix any type 
+let mixedType: [number, string, string]
+mixedType : [2, "four", "six"]
